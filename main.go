4082d4b0d3f4df31467c39ba264f2c4b8620647c
blazejkrzak <blazej@m8b.pl>
2021-02-05 13:39:15 +0100

    Feature: IPC from ENV

--- main.go
+++ main.go
@@ -1,14 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 // Motivation of this repository is to have TX Pool filled with insane numbers in geth.
 // For now it will be just only a spike that makes the work, if possible it will be refactored and polished.
 // It should be designed to work especially in docker and kubernetes environment, but tests at least in unit/component
 // level should be runnable without containerisation.
 
-func init() {
+var (
+	IPC_ENDPOINT = "./geth.ipc"
+)
 
+func init() {
+	ipcEndpoint := os.Getenv("IPC_ENDPOINT")
+
+	if "" != ipcEndpoint {
+		IPC_ENDPOINT = ipcEndpoint
+	}
 }
 
 func main() {
-
+	fmt.Printf("\n Running chaindriller on IPC: %s", IPC_ENDPOINT)
 }
