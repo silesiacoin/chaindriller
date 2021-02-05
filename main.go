2e03e9b227b9352422429dd62fac11831a5144c6
blazejkrzak <blazej@m8b.pl>
2021-02-05 14:23:54 +0100

    Feature: added ethereum client to attach to ipc

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-ethereum/ethclient"
+	"math/big"
 	"os"
+	"strconv"
 )
 
 // Motivation of this repository is to have TX Pool filled with insane numbers in geth.
@@ -11,17 +14,41 @@ import (
 // level should be runnable without containerisation.
 
 var (
-	IPC_ENDPOINT = "./geth.ipc"
+	IpcEndpoint    = "./geth.ipc"
+	ChainId        = big.NewInt(1)
+	ethereumClient *ethclient.Client
 )
 
 func init() {
 	ipcEndpoint := os.Getenv("IPC_ENDPOINT")
+	chainId := os.Getenv("CHAIN_ID")
 
 	if "" != ipcEndpoint {
-		IPC_ENDPOINT = ipcEndpoint
+		IpcEndpoint = ipcEndpoint
 	}
+
+	chainIdInt, err := strconv.ParseInt(chainId, 10, 64)
+
+	if nil == err && chainIdInt != ChainId.Int64() {
+		ChainId = big.NewInt(chainIdInt)
+	}
+
+	if nil != err {
+		fmt.Printf("\n %v is not a valid int, defaulting to %d err: %s \n", chainId, ChainId, err.Error())
+	}
+
+	ethereumClient = newClient(IpcEndpoint)
 }
 
 func main() {
-	fmt.Printf("\n Running chaindriller on IPC: %s", IPC_ENDPOINT)
+	fmt.Printf("\n Running chaindriller on IPC: %s", IpcEndpoint)
+}
+
+// newClient creates a client with specified remote URL.
+func newClient(ipcEndpoint string) *ethclient.Client {
+	client, err := ethclient.Dial(ipcEndpoint)
+	if err != nil {
+		panic(fmt.Sprintf("Could not connect to ethereum node url: %s, Err: %s", ipcEndpoint, err.Error()))
+	}
+	return client
 }
