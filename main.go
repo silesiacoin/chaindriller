5b4895325723ff5fd087a290fd1b35ff30e51d9a
blazejkrzak <blazej@m8b.pl>
2021-02-05 13:36:49 +0100

    Added MVP goal

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
 package main
+
+// Motivation of this repository is to have TX Pool filled with insane numbers in geth.
+// For now it will be just only a spike that makes the work, if possible it will be refactored and polished.
+// It should be designed to work especially in docker and kubernetes environment, but tests at least in unit/component
+// level should be runnable without containerisation.
+
+func init() {
+
+}
+
+func main() {
+
+}
